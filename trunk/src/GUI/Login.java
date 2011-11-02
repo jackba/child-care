d889471bc41b3d672dee30355857e1297c535dea
pviet18@gmail.com <pviet18@gmail.com@7b50d76b-702c-47d2-0f96-b0069ba6ed1b>
2011-11-02 22:27:40 +0000

    edit form login

--- trunk/src/GUI/Login.java
+++ trunk/src/GUI/Login.java
@@ -9,16 +9,37 @@
  * Created on Nov 5, 2011, 1:31:49 AM
  */
 package GUI;
-
+import DAO.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import javax.swing.JFrame;
+import java.sql.*;
+import java.sql.ResultSet;
+import java.util.Vector;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.ComboBoxModel;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
 /**
  *
  * @author mr duy
  */
 public class Login extends javax.swing.JFrame {
-
+    public static String username;
+    public static String password;
+    public static Login login;
+    public static boolean logIsAdmin;
+    Connection conn;
+    Resultset rs=null;
+    PreparedStatement pstmt;
+    Statement st;
     /** Creates new form Login */
     public Login() {
         initComponents();
+        
     }
 
     /** This method is called from within the constructor to
@@ -36,7 +57,7 @@ public class Login extends javax.swing.JFrame {
         lblPassWord = new javax.swing.JLabel();
         lblPermission = new javax.swing.JLabel();
         txtUserName = new javax.swing.JTextField();
-        PassWord = new javax.swing.JPasswordField();
+        txtPassWord = new javax.swing.JPasswordField();
         jComboBox1 = new javax.swing.JComboBox();
         btnLogin = new javax.swing.JButton();
         btnExit = new javax.swing.JButton();
@@ -48,31 +69,31 @@ public class Login extends javax.swing.JFrame {
 
         jPanel1.setBackground(new java.awt.Color(255, 255, 255));
 
-        lblTittle.setFont(new java.awt.Font("Tahoma", 1, 36)); // NOI18N
+        lblTittle.setFont(new java.awt.Font("Tahoma", 1, 36));
         lblTittle.setForeground(new java.awt.Color(0, 0, 255));
         lblTittle.setText("LOGIN");
 
-        lblUserName.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        lblUserName.setFont(new java.awt.Font("Tahoma", 0, 14));
         lblUserName.setForeground(new java.awt.Color(0, 0, 255));
         lblUserName.setText("UserName:");
 
-        lblPassWord.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        lblPassWord.setFont(new java.awt.Font("Tahoma", 0, 14));
         lblPassWord.setForeground(new java.awt.Color(0, 0, 255));
         lblPassWord.setText("PassWord:");
         lblPassWord.setToolTipText("");
 
-        lblPermission.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        lblPermission.setFont(new java.awt.Font("Tahoma", 0, 14));
         lblPermission.setForeground(new java.awt.Color(0, 0, 255));
         lblPermission.setText("Permission:");
 
-        txtUserName.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        txtUserName.setFont(new java.awt.Font("Tahoma", 0, 14));
         txtUserName.setForeground(new java.awt.Color(0, 0, 255));
 
-        PassWord.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
-        PassWord.setForeground(new java.awt.Color(0, 0, 255));
-        PassWord.addActionListener(new java.awt.event.ActionListener() {
+        txtPassWord.setFont(new java.awt.Font("Tahoma", 0, 14));
+        txtPassWord.setForeground(new java.awt.Color(0, 0, 255));
+        txtPassWord.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                PassWordActionPerformed(evt);
+                txtPassWordActionPerformed(evt);
             }
         });
 
@@ -82,6 +103,11 @@ public class Login extends javax.swing.JFrame {
         btnLogin.setForeground(new java.awt.Color(255, 0, 0));
         btnLogin.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Image/unlock.png"))); // NOI18N
         btnLogin.setText("LOGIN");
+        btnLogin.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnLoginActionPerformed(evt);
+            }
+        });
 
         btnExit.setForeground(new java.awt.Color(255, 0, 0));
         btnExit.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Image/Exit.png"))); // NOI18N
@@ -90,6 +116,11 @@ public class Login extends javax.swing.JFrame {
         btnReset.setForeground(new java.awt.Color(255, 0, 0));
         btnReset.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Image/reset.png"))); // NOI18N
         btnReset.setText("RESET");
+        btnReset.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnResetActionPerformed(evt);
+            }
+        });
 
         jLabel5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Image/icon_login.gif"))); // NOI18N
 
@@ -117,7 +148,7 @@ public class Login extends javax.swing.JFrame {
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
                             .addComponent(txtUserName, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 170, Short.MAX_VALUE)
                             .addComponent(jComboBox1, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, 106, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(PassWord, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, 196, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                            .addComponent(txtPassWord, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, 196, javax.swing.GroupLayout.PREFERRED_SIZE)))
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGap(38, 38, 38)
                         .addComponent(btnReset)
@@ -145,7 +176,7 @@ public class Login extends javax.swing.JFrame {
                                     .addComponent(lblUserName))
                                 .addGap(18, 18, 18)
                                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                                    .addComponent(PassWord, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(txtPassWord, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                                     .addComponent(lblPassWord))
                                 .addGap(18, 18, 18)
                                 .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
@@ -170,16 +201,89 @@ public class Login extends javax.swing.JFrame {
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 355, Short.MAX_VALUE)
                 .addContainerGap())
         );
 
-        pack();
+        java.awt.Dimension screenSize = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
+        setBounds((screenSize.width-549)/2, (screenSize.height-404)/2, 549, 404);
     }// </editor-fold>//GEN-END:initComponents
 
-private void PassWordActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_PassWordActionPerformed
+private void txtPassWordActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtPassWordActionPerformed
 // TODO add your handling code here:
-}//GEN-LAST:event_PassWordActionPerformed
+    btnLoginActionPerformed(evt);
+}//GEN-LAST:event_txtPassWordActionPerformed
+
+    private void btnResetActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnResetActionPerformed
+        // TODO add your handling code here:
+        txtUserName.setText("");
+       txtPassWord.setText("");
+       txtUserName.requestFocus();
+    }//GEN-LAST:event_btnResetActionPerformed
+public void callConnection(){
+     try {
+            conn = DAO.DAOUtil.getConnection();
+            username = txtUserName.getText();
+            password = new String(txtPassWord.getPassword());
+             if (txtUserName.getText().equals("")) {
+                JOptionPane.showMessageDialog(this, "Please,Enter UserName");
+                txtUserName.requestFocus();
+                return;
+            }
+            if (txtPassWord.getPassword().equals("")) {
+                JOptionPane.showMessageDialog(this, "Please,Enter Password");
+                txtPassWord.requestFocus();
+                return;
+            }
+             if(jComboBox1.getSelectedItem().equals("Admin")){
+          logIsAdmin = true;
+           try {
+              //  @SuppressWarnings("static-access")
+                
+                conn = DAO.DAOUtil.getConnection();
+                if(conn!=null){
+                //creating a string to store the command T-SQL:
+                    
+                String strsql="select UserName,Password from tblUser "
+                                + "where UserName=? and PassWord=?";
+                pstmt = conn.prepareStatement(strsql);
+                pstmt.setString(1, username);
+                pstmt.setString(2, password);
+                rs =    (Resultset) pstmt.executeQuery();
+                if(rs.next()){
+                 //   InformationAdmin.setStatus("1");
+                    this.setVisible(false);
+                    new MainFormAdmin().setVisible(true);
+                    this.dispose();
+                }
+                else{
+                    JOptionPane.showMessageDialog(this, "Username or password fail!");
+                    txtPassWord.setText("");
+                    txtUserName.requestFocus();
+                    txtUserName.selectAll();
+                }
+                }
+                else
+                {
+                    new ServerConfig().setVisible(true);
+                    this.dispose();
+                }
+             } catch (SQLException ex) {
+                    ex.printStackTrace();
+                    JOptionPane.showMessageDialog(this, "Cannot connect to server!",
+                                    "Error", JOptionPane.ERROR_MESSAGE);
+                    return;
+             }
+
+        }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "");
+        }
+}
+    private void btnLoginActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnLoginActionPerformed
+        // TODO add your handling code here:
+       
+    }//GEN-LAST:event_btnLoginActionPerformed
 
     /**
      * @param args the command line arguments
@@ -217,7 +321,6 @@ private void PassWordActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRS
         });
     }
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JPasswordField PassWord;
     private javax.swing.JButton btnExit;
     private javax.swing.JButton btnLogin;
     private javax.swing.JButton btnReset;
@@ -228,6 +331,7 @@ private void PassWordActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRS
     private javax.swing.JLabel lblPermission;
     private javax.swing.JLabel lblTittle;
     private javax.swing.JLabel lblUserName;
+    private javax.swing.JPasswordField txtPassWord;
     private javax.swing.JTextField txtUserName;
     // End of variables declaration//GEN-END:variables
 }
